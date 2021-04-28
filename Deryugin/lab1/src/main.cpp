453eb13b3060c0c429933bce3855d20b7f7e4832
dmitryduy <cdcvh14@gmail.com>
2021-04-28 15:17:46 +0300

    lab1 done

---
+++ Deryugin/lab1/src/main.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <stack>
+
+bool IS_SHOW_INTERMEDIATE_RESULTS = false;
+//array of names of squares
+const char namesOfSquares[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8','9', 'a', 'b', 'c', 'd', 'e', 'i', 'f'};
+
+class Square {
+public:
+    int posX, posY, width;
+    bool isMain;
+    Square(int x, int y, int w, bool isMain=false) {
+        posX = x;
+        posY = y;
+        width = w;
+        this->isMain = isMain;
+    }
+};
+
+class Desk {
+public:
+    int minSquares = 9999999;
+    int currentSquare = 1;
+    std::stack<Square> minArrayOfSquare;
+    std::stack<Square> arrayOfSquares;
+    int countOfOperation = 0;
+};
+
+Square findMaxSquare(int k , int x, int y, int** matrix) {
+    int width = 0;
+    int startWith;
+
+    if (x > y) startWith = x;
+    else startWith = y;
+
+    for (int i = startWith; i < k; i++) {
+        if (matrix[x + width][y + width] == 0 && matrix[x + width][y] == 0 && matrix[x][y + width] == 0) {
+            width++;
+        } else break;
+    }
+    return {x, y, width};
+}
+
+void fillMatrix(int** matrix, Square square, Desk* desk) {
+    for (int i = square.posX; i < square.posX + square.width; i++) {
+        for (int j = square.posY; j < square.posY + square.width; j++) {
+            matrix[i][j] = desk->currentSquare;
+        }
+    }
+    desk->currentSquare++;
+}
+
+void backtracking(int k , int** matrix, Desk* desk, bool specialCase=false) {
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                desk->countOfOperation++;
+                if (matrix[i][j] != 0) continue;
+                // if current count of squares more than previous count
+                if (desk->currentSquare >= desk->minSquares) return;
+
+                Square square = findMaxSquare(k, i, j, matrix);
+                if (specialCase) desk->minArrayOfSquare.push(square);
+                else desk->arrayOfSquares.push(square);
+                fillMatrix(matrix, square, desk);
+            }
+        }
+
+
+}
+
+bool decreaseSquare(int k , int **matrix, Desk* desk) {
+    // change minimum count of squares
+    if (desk->currentSquare < desk->minSquares) {
+        desk->minArrayOfSquare = desk->arrayOfSquares;
+        desk->minSquares = desk->currentSquare;
+    }
+
+    //remove squares with side equal 1
+    while (desk->arrayOfSquares.top().width == 1 ) {
+        desk->countOfOperation++;
+        Square top = desk->arrayOfSquares.top();
+        matrix[top.posX][top.posY] = 0;
+        desk->arrayOfSquares.pop();
+        desk->currentSquare--;
+    }
+
+
+    Square top = desk->arrayOfSquares.top();
+
+    if (top.isMain) return true;
+    // decrease square side by 1
+    for (int i = 0; i < top.width; i++) {
+        desk->countOfOperation++;
+        matrix[top.posX + i][top.posY + top.width - 1] = 0;
+        matrix[top.posX + top.width - 1][top.posY + i] = 0;
+    }
+    desk->arrayOfSquares.top().width--;
+    return false;
+
+}
+
+
+void printOptimalDesk(int** matrix, std::stack<Square> squares) {
+    // print intermediate results
+    if (IS_SHOW_INTERMEDIATE_RESULTS) {
+        int numberOfSquare = 1;
+        while (!squares.empty()) {
+            Square top = squares.top();
+            for (int i = top.posX; i < top.posX + top.width; i++) {
+                for (int j = top.posY; j < top.posY + top.width; j++) {
+                    matrix[i][j] = numberOfSquare;
+                }
+            }
+            numberOfSquare++;
+            squares.pop();
+        }
+    }
+    //print answer
+    else {
+        int count = squares.size();
+        std::cout<<count<<std::endl;
+
+        while (!squares.empty()) {
+                std::cout<<squares.top().posX + 1<<" "<<squares.top().posY + 1<<" "<<squares.top().width<<std::endl;
+
+            squares.pop();
+        }
+    }
+
+}
+
+void prepare(int k, int**matrix) {
+    Desk desk = Desk();
+
+    // even side
+    if (k % 2 == 0) {
+        std::cout<<"k % 2 == 0 => min count of square = 4 and side of this squares = k / 2\n";
+        Square square(0, 0, k / 2);
+        desk.minArrayOfSquare.push(square);
+        fillMatrix(matrix, square, &desk);
+        backtracking(k, matrix, &desk, true);
+    }
+    // side multiple of 3
+    else if (k % 3 == 0) {
+        std::cout<<"k % 3 == 0 => min count of square = 3 and side of the largest square = k / 3 * 2\n";
+        Square square(0, 0, k / 3 * 2);
+        desk.minArrayOfSquare.push(square);
+        fillMatrix(matrix, square, &desk);
+        backtracking(k, matrix, &desk, true);
+    }
+    //side multiple of 5
+    else if (k % 5 == 0) {
+        std::cout<<"k % 5 == 0 => min count of square = 5 and side of the largest square = k / 5 * 3\n";
+        Square square(0, 0, k / 5 * 3);
+        desk.minArrayOfSquare.push(square);
+        fillMatrix(matrix, square, &desk);
+        backtracking(k, matrix, &desk, true);
+    }
+    //otherwise
+    else {
+        //create 3 main squares
+        Square squareMax(0,0, k / 2 + 1, true);
+        Square squareMin1(0, k / 2 + 1, k / 2, true);
+        Square squareMin2(k / 2 + 1, 0, k / 2, true);
+        desk.arrayOfSquares.push(squareMax);
+        fillMatrix(matrix, squareMax, &desk);
+        desk.arrayOfSquares.push(squareMin1);
+        fillMatrix(matrix, squareMin1, &desk);
+        desk.arrayOfSquares.push(squareMin2);
+        fillMatrix(matrix, squareMin2, &desk);
+
+        while (true) {
+            backtracking(k, matrix, &desk);
+            if (IS_SHOW_INTERMEDIATE_RESULTS) {
+                std::cout<<"--------------------------\n";
+                for (int i = 0; i < k; i++) {
+                    for (int j = 0; j < k; j++) {
+                        std::cout<<matrix[i][j]<<" ";
+                    }
+                    std::cout<<"\n";
+                }
+                std::cout<<"--------------------------\n";
+            }
+            if(decreaseSquare(k, matrix, &desk)) break;
+        }
+    }
+
+
+
+    printOptimalDesk(matrix, desk.minArrayOfSquare);
+    // print desk if IS_SHOW_INTERMEDIATE_RESULTS = true
+    if (IS_SHOW_INTERMEDIATE_RESULTS) {
+        std::cout<<"Optimal desk:\n";
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                std::cout<<namesOfSquares[matrix[i][j]]<<" ";
+            }
+            std::cout<<"\n";
+        }
+
+    }
+
+    std::cout<<"Count of operation with side square equal "<<k<<" - "<<desk.countOfOperation + k * k<<std::endl;
+    desk.countOfOperation = 0;
+}
+
+
+int main() {
+    int k;
+    std::cin>>k;
+    //create matrix
+    int **matrix = new int*[k];
+    for (int i = 0; i < k; i++) {
+        matrix[i] = new int[k];
+    }
+
+    //fill matrix
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+    prepare(k, matrix);
+
+    //free mem
+    for (int i = 0; i < k; i++) {
+        delete matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
